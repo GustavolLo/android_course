b8cc7488025223380dfb7da8adc6b270b0dce50e
GustavolLo <goodluke111931@gmail.com>
2021-01-09 23:24:24 +0100

    Adding cancel menu item.

--- app/src/main/java/com/ex/notekeeper/NoteActivity.java
+++ app/src/main/java/com/ex/notekeeper/NoteActivity.java
@@ -23,6 +23,25 @@ public class NoteActivity extends AppCompatActivity {
     private Spinner mSpinnerCourses;
     private EditText mTextNoteTitle;
     private EditText mTextNoteText;
+    private int mNotePosition;
+    private boolean mIsCancelling;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mIsCancelling) {
+            if(mIsNewNote)
+            DataManager.getInstance().removeNote(mNotePosition);
+        } else {
+            saveNote();
+        }
+    }
+
+    private void saveNote() {
+        mNote.setCourse((CourseInfo) mSpinnerCourses.getSelectedItem());
+        mNote.setTitle(mTextNoteTitle.getText().toString());
+        mNote.setText(mTextNoteText.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,9 +77,17 @@ public class NoteActivity extends AppCompatActivity {
         Intent intent = getIntent();
         int position = intent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET);
         mIsNewNote = position == POSITION_NOT_SET;
-        if(!mIsNewNote) {
+        if(mIsNewNote) {
+           createNewNote();
+        } else {
             mNote = DataManager.getInstance().getNotes().get(position);
         }
+    }
+
+    private void createNewNote() {
+        DataManager dm = DataManager.getInstance();
+        mNotePosition = dm.createNewNote();
+        mNote = dm.getNotes().get(mNotePosition);
     }
 
     @Override
@@ -81,6 +108,9 @@ public class NoteActivity extends AppCompatActivity {
         if (id == R.id.action_send_mail) {
             sendEmail();
             return true;
+        } else if(id == R.id.action_cancel) {
+            mIsCancelling = true;
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
